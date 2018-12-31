e01c5234af70b839a05411a3f285299621832c6e
MarounMaroun <maroun.bassam@gmail.com>
2018-12-31 10:10:50 +0200

    Change squares ASCII

--- mycontribution.sh
+++ mycontribution.sh
@@ -23,18 +23,21 @@ i=0
 while [[ "$i" -le 6 ]]; do
     for (( n="$i";n<${#graph[@]};n+=7 )); do
         if [[ "${graph[$n]}" == "#c6e48b" ]]; then
-            printf "\\e[38;5;71m▇\\e[0m|"
+            printf "\\e[38;5;71m▣\\e[0m|"
         elif [[ "${graph[$n]}" == "#7bc96f" ]]; then
-            printf "\\e[38;5;64m▇\\e[0m|"
+            printf "\\e[38;5;64m▣\\e[0m|"
         elif [[ "${graph[$n]}" == "#239a3b" ]]; then
-            printf "\\e[38;5;22m▇\\e[0m|"
+            printf "\\e[38;5;22m▣\\e[0m|"
         elif [[ "${graph[$n]}" == "#196127" ]]; then
-            printf "\\e[38;5;234m▇\\e[0m|"
+            printf "\\e[38;5;238m▣\\e[0m|"
         # no contribution
         elif [[ "${graph[$n]}" == "#ebedf0" ]]; then
-            printf "\\e[38;5;252m▇\\e[0m|"
+            printf "\\e[38;5;252m▣\\e[0m|"
         fi
     done
     echo
     (( i++ ))
 done
+echo
+printf "Less \\e[38;5;252m▣\\e[0m|\\e[38;5;71m▣\\e[0m|\\e[38;5;64m▣\\e[0m|\\e[38;5;22m▣\\e[0m|\\e[38;5;238m▣\\e[0m More"
+echo
